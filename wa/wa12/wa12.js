213edcc2621f0cde6a5f58f848d50b4e4d75a6e9
joelpearce01 <jope5931@colorado.edu>
2025-04-13 16:42:51 -0700

    webassignment12 completed

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,71 +1,85 @@
-var newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
-var answerBtn = document.querySelector('#js-tweet').addEventListener('click', function() {
-    displayAnswer(current.genre);
-});
-
-var endpointgenre = "https://binaryjazz.us/wp-json/genrenator/v1/";
-// https://github.com/cyberboysumanjay/GaanaAPI
 var endpointimage = "https://api.thecatapi.com/v1/images/search";
+var endpointquote = "https://api.breakingbadquotes.xyz/v1/quotes";
 
+api_key = 'live_fVhlpNuf0kDfvLojU2iVslXmQfVwq2RlrOwRkxlxLcSuP2ZfK1x8Os29nuNHWBDZ';
 
 let current = {
-    question: '',
-    adjectives: '',
-    genre: ''
+    imageurl: '',
+    alt: '',
+    quote: ''
 };
 
-async function getQuote() {
-    try {
-        const response = await fetch(endpointgenre);
-        const catimage = await fetch(endpointimage);
+// buttons
+document.querySelector('#js-new-image').addEventListener('click', getImage);
+document.querySelector('#js-quote').addEventListener('click', getQuote);
 
-        if (!response.ok) {
-            throw Error(response.statusText);
-        };
+//fetching functions
+async function getImage() {
+    try {
+        const catimage = await fetch(endpointimage);
 
         if (!catimage.ok) {
             throw Error(catimage.statusTest());
         };
 
-        const json = await response.json();
-        const cat = await response.json();
+        const catdata = await catimage.json();
+        const imageObj = catdata[0];
 
-        displayQuote(json.instruments);
-        displayQuote(json.adjectives);
-        displayQuote(json.genre);
-        displayImage(cat.url)
+        current.imageurl = imageObj.url;
+        current.alt = imageObj.id;
+        displayImage(current.imageurl, current.alt);
 
-        console.log(json.genre);
-        console.log(json.adjectives);
-
-        current.question = json.instruments;
-        current.adjectives = json.adjectives;
-        current.genre = json.genres;
+        console.log(caturl[0].url);
+        console.log(caturl[1]);
 
     } catch(err) {
-        console.log(err);
-        alert('fail');
+        console.log('image: fail', err);
     };
-    console.log('help oh god please send help');
-}
+};
 
-function displayQuote(quote) {
-    const quoteText = document.querySelector('#js-quote-text');
-    quoteText.textContent = quote;
-}
+async function getQuote() {
+    try {
+        const response = await fetch(endpointquote);
 
-function displayImage(image) {
-    const newimage = document.createElement(img);
-    newimage.src = image;
-    newimage.width = image.width;
-    newimage.height = image.height;
-    const imagecontainer = document.createElementById('image-container');
-    imagecontainer.appendChild(newimage);
-}
+        if (!response.ok) {
+            throw Error(response.statusTest());
+        };
 
-function displayAnswer(answer) {
-    const answerText = document.querySelector('#js-answer-text');
-    answerText.textContent = answer;
-}
+        const bbquote = await response.json();
 
-getQuote();
+        current.quote = bbquote[0].quote;
+
+        console.log(bbquote[0].quote);
+
+        displayImage(current.imageurl, current.alt);
+
+    } catch(err) {
+        console.log('quote: fail', err);
+    };
+};
+
+//display functions
+function displayImage(url, id) {
+    const container = document.querySelector("#image-container");
+    container.innerHTML = '';
+    
+    const Wrapper = document.createElement("div");
+    Wrapper.classList.add("Wrapper");
+
+    const catImgUrl = document.createElement("img");
+    catImgUrl.setAttribute('src',`${current.imageurl}`);
+    catImgUrl.setAttribute('alt', `Cat ID: ${current.alt}`);
+    catImgUrl.classList.add("showcase");
+
+    const quoteText = document.createElement("div");
+    quoteText.classList.add("bbnoquote");
+    quoteText.textContent = current.quote;
+
+    Wrapper.appendChild(catImgUrl);
+    Wrapper.appendChild(quoteText);
+    container.append(Wrapper);
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    getQuote().then(() => getImage());
+  });
